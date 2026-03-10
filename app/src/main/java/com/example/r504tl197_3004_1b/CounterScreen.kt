1896905340c60891d387163fc6bda699fe500334
juhani.kuru <juhanialeksikuru@gmail.com>
2026-03-10 19:23:06 +0200

    -add Nollaa laskuri Button

--- app/src/main/java/com/example/r504tl197_3004_1b/CounterScreen.kt
+++ app/src/main/java/com/example/r504tl197_3004_1b/CounterScreen.kt
@@ -2,6 +2,7 @@ package com.example.r504tl197_3004_1b
 
 import android.util.Log
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -25,8 +26,10 @@ fun CounterRoot(modifier: Modifier = Modifier) {
         mutableStateOf(0)
     }
 
-    Counter(count = count.value, onClick = {
+    Counter(count = count.value, addCount = {
         count.value++
+    }, startOver = {
+        count.value = 0
     })
 }
 
@@ -35,7 +38,12 @@ fun CounterRoot(modifier: Modifier = Modifier) {
 // voi halutessasi käyttää mutableIntStateOf:ia myös tässä. Compose ymmärtää, että count on integer arvo
 // mutableStateOfista on olemassa integereille oma overload (mutableIntStateOf())
 @Composable
-fun Counter(modifier: Modifier = Modifier, count: Int, onClick : () -> Unit) {
+fun Counter(
+    modifier: Modifier = Modifier,
+    count: Int,
+    addCount: () -> Unit,
+    startOver: () -> Unit
+) {
 
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -43,14 +51,22 @@ fun Counter(modifier: Modifier = Modifier, count: Int, onClick : () -> Unit) {
 
 
     ) {
-        Button(onClick = {
-            // kun muuttujan arvo laitetaan mutableStateOfiin, sitä ei voi suoraan enää kasvattaa
-            // vaan kasvatetaan sen valueta
-            onClick()
+        Column {
+            Button(onClick = {
+                // kun muuttujan arvo laitetaan mutableStateOfiin, sitä ei voi suoraan enää kasvattaa
+                // vaan kasvatetaan sen valueta
+                addCount()
 
-        }) {
-            Text("count: $count")
+            }) {
+                Text("count: $count")
+            }
+            Button(onClick = {
+                startOver()
+            }) {
+                Text("Nollaa laskuri")
+            }
         }
+
     }
 }
 
